05a3a7b11f38d98f20e86d48575e2c42bc2f25d3
Pierre COURANT <pcourant.dev@gmail.com>
2022-02-14 09:16:21 +0100

    fix(issue3): Add cutom validation control message on form inputs

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,6 +16,73 @@ const formData = document.querySelectorAll(".formData");
 const closeBg = document.querySelector(".close");
 const submitBg = document.querySelector(".btn-submit");
 
+// INPUT items
+const firstName = {
+  element: document.getElementById("first"),
+  messageInvalid:
+    "Veuillez entrer 2 caractères ou plus pour le champ du prénom.",
+};
+const lastName = {
+  element: document.getElementById("last"),
+  messageInvalid: "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+};
+const email = {
+  element: document.getElementById("email"),
+  messageInvalid: "Veuillez entrer un email valide : jean.dupont@email.com",
+};
+const birthdate = {
+  element: document.getElementById("birthdate"),
+  messageInvalid: "Vous devez entrer votre date de naissance.",
+};
+const quantity = {
+  element: document.getElementById("quantity"),
+  messageInvalid: "Vous devez entrer un nombre entre 0 et 99.",
+};
+const location1 = {
+  element: document.getElementById("location1"),
+  messageInvalid: "Vous devez choisir un tournoi.",
+};
+const location2 = {
+  element: document.getElementById("location2"),
+  messageInvalid: "Vous devez choisir un tournoi.",
+};
+const location3 = {
+  element: document.getElementById("location3"),
+  messageInvalid: "Vous devez choisir un tournoi.",
+};
+const location4 = {
+  element: document.getElementById("location4"),
+  messageInvalid: "Vous devez choisir un tournoi.",
+};
+const location5 = {
+  element: document.getElementById("location5"),
+  messageInvalid: "Vous devez choisir un tournoi.",
+};
+const location6 = {
+  element: document.getElementById("location6"),
+  messageInvalid: "Vous devez choisir un tournoi.",
+};
+const checkbox1 = {
+  element: document.getElementById("checkbox1"),
+  messageInvalid:
+    "Vous devez vérifier que vous acceptez les termes et conditions.",
+};
+
+const inputs = [
+  firstName,
+  lastName,
+  email,
+  birthdate,
+  quantity,
+  location1,
+  location2,
+  location3,
+  location4,
+  location5,
+  location6,
+  checkbox1,
+];
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -35,4 +102,87 @@ function launchModal(event) {
 function closeModal(event) {
   // remove display class to close the form pop-up
   modalbg.classList.remove("bground--displayed");
+}
+
+// Add input control validation on event "change"
+for (const input of inputs) {
+  // input.element.addEventListener("change", function (event) {
+  //   if (!input.element.validity.valid) {
+  //     input.element.setCustomValidity("");
+  //     input.element.parentNode.setAttribute("data-error", input.messageInvalid);
+  //     input.element.parentNode.setAttribute("data-error-visible", "true");
+  //   } else {
+  //     // It's vital to set the message to an empty string if there are no errors
+  //     // As long as the error message is not empty, the form will not pass validation and will not be submitted.
+  //     // input.setCustomValidity("");
+  //     input.element.parentNode.removeAttribute("data-error");
+  //     input.element.parentNode.removeAttribute("data-error-visible");
+  //   }
+  // });
+  // input.element.addEventListener("invalid", function (event) {
+  //   if (!input.element.validity.valid) {
+  //     // input.element.setCustomValidity("");
+  //     // input.element.reportValidity();
+  //     input.element.parentNode.setAttribute("data-error", input.messageInvalid);
+  //     input.element.parentNode.setAttribute("data-error-visible", "true");
+  //   } else {
+  //     // It's vital to set the message to an empty string if there are no errors
+  //     // As long as the error message is not empty, the form will not pass validation and will not be submitted.
+  //     // input.setCustomValidity("");
+  //     input.element.parentNode.removeAttribute("data-error");
+  //     input.element.parentNode.removeAttribute("data-error-visible");
+  //   }
+  // });
+
+  input.element.addEventListener(
+    "change",
+    (function () {
+      return function (e) {
+        //prevent the browser from showing default error bubble / hint
+        e.preventDefault();
+
+        if (!input.element.validity.valid) {
+          input.element.parentNode.setAttribute(
+            "data-error",
+            input.messageInvalid
+          );
+          input.element.parentNode.setAttribute("data-error-visible", "true");
+        } else {
+          // It's vital to set the message to an empty string if there are no errors
+          // As long as the error message is not empty, the form will not pass validation and will not be submitted.
+          // input.setCustomValidity("");
+          input.element.parentNode.removeAttribute("data-error");
+          input.element.parentNode.removeAttribute("data-error-visible");
+        }
+      };
+    })(),
+    true
+  );
+
+  input.element.addEventListener(
+    "invalid",
+    (function () {
+      return function (e) {
+        //prevent the browser from showing default error bubble / hint
+        e.preventDefault();
+
+        if (!input.element.validity.valid) {
+          // input.element.setCustomValidity("");
+          // input.element.reportValidity();
+          input.element.parentNode.setAttribute(
+            "data-error",
+            input.messageInvalid
+          );
+          input.element.parentNode.setAttribute("data-error-visible", "true");
+        } else {
+          // It's vital to set the message to an empty string if there are no errors
+          // As long as the error message is not empty, the form will not pass validation and will not be submitted.
+          // input.setCustomValidity("");
+          input.element.parentNode.removeAttribute("data-error");
+          input.element.parentNode.removeAttribute("data-error-visible");
+        }
+      };
+    })(),
+    true
+  );
 }
